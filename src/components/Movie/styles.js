b60aef08b32947866f91b1cfa40ca17e5e75ee12
praveenbhat73 <bhatpraveen721@gmail.com>
2022-10-01 17:23:39 +0530

    commit

--- src/components/Movie/styles.js
+++ src/components/Movie/styles.js
@@ -36,15 +36,15 @@ export default makeStyles((theme) => ({
   },
   image: {
 
-    borderRadius:'20px',
-    height: '330px',
+    borderRadius:'5px',
+    height: '310px',
     marginBottom: '12px',
     // boxShadow: 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
     // boxShadow: '3px 3px 3px 3px rgba(0, 0, 0, 0.2)',
-    boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px;',
+    // boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px;',
     '&:hover': {
-      transform: 'scale(1.05)',
-      boxShadow:' rgba(0, 0, 0, 0.4) 0px 30px 90px;'
+      transform: 'scale(1.06)',
+      boxShadow:' rgba(0, 0, 0, 0.2) 0px 5px;'
   
     },
   },
