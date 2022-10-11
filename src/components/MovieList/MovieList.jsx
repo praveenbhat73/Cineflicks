53d60ae92953e1e2994bfff75232741591f0e8dd
Praveen Narayan Bhat <108626052+praveenbhat73@users.noreply.github.com>
2022-10-11 17:07:36 +0530

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import {Grid} from '@mui/material'
 import useStyles from './styles'
 import {Movie} from '..'
-const MovieList = ({movies,numberofMovies,excludeFirst}) => {
-  const startFrom=excludeFirst?1:0;
+const MovieList = ({movies,numberofMovies}) => {
+  // const startFrom=excludeFirst?1:0;
      const classes=useStyles();
   return (
    
       <Grid container className={classes.moviesContainer}>
        {
-        movies.results.slice(startFrom,numberofMovies).map((movie,i) => (
+        movies.results.slice(0,numberofMovies).map((movie,i) => (
           <Movie key={i} movie={movie} i={i}/>
         ))
        }
@@ -19,3 +19,4 @@ const MovieList = ({movies,numberofMovies,excludeFirst}) => {
 }
 
 export default MovieList
+
